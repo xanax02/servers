2fab4d0cf99df5cf884564c3c2e46b56084f3b8b
abhay <abhaytxanax@gmail.com>
2023-02-28 02:27:08 +0530

    mogo-schema

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url); //for grabbing the file url specifically when using modules
@@ -32,6 +33,9 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage});
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
